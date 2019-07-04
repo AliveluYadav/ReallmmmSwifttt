b5b30811f7fc6111819c8e3dbe12e4aa808cb074
Alivelu <alivelu7193@gmail.com>
2019-07-04 12:48:33 +0530

    first commit

---
+++ ReallmmmSwifttt/SaveData.swift
@@ -0,0 +1,19 @@
+//
+//  SaveData.swift
+//  ReallmmmSwifttt
+//
+//  Created by LetMeCall Corp on 06/12/18.
+//  Copyright © 2018 LetMeCall Corp. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Human:Object{
+    
+    @objc dynamic var userName = ""
+    @objc dynamic var password = ""
+    @objc dynamic var saveArry = [String]()
+    
+    
+}
--- ReallmmmSwifttt/ViewController.swift
+++ ReallmmmSwifttt/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var userTF: UITextField!
+    @IBOutlet weak var pswdTF: UITextField!
+    var arry = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    @IBAction func clickOnSave(_ sender: Any) {
+    
+      let mike = Human()
+        mike.userName = self.userTF.text!
+        mike.password = self.pswdTF.text!
+        
+        let realm = try! Realm()
+        try! realm.write {
+            realm.add(mike)
+            self.arry.append(self.userTF.text!)
+            print("added \(mike.userName) to Realm")
+            
+           
+        }
+        
+        print("array\(self.arry)")
+    }
+    
+    
 
 }
 
